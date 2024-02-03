175203c59663f029328cdb9f4e02dbcae6e665e3
victorsousalima <victorsousa16042005@gmail.com>
2024-02-03 12:42:55 -0300

    First commit

---
+++ src/main/java/br/com/victor/TesteTgidApplication.java
@@ -0,0 +1,13 @@
+package br.com.victor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTgidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTgidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/victor/TesteTgidApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTgidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
